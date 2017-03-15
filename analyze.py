709533b1075d33bf862c02c92a8e751563330cff
Charles Lu <charleslu@live.com>
2017-03-15 14:03:46 -0700

    Drop data with negative 'top'

--- analyze.py
+++ analyze.py
@@ -26,21 +26,35 @@ def load_data():
 
     return attributes, profiles, layers
 
-def visualize_layers():
+def visualize_layers(layers):
     plt.figure()
     plt.scatter(layers.head(5000)['top'], layers.head(5000)['orgc_value_avg'])
     plt.show()
 
+def drop_bad_data(layers, profiles):
+    bad_layers = layers[layers['top'] < 0]
+    bad_profiles = pd.merge(profiles, bad_layers, on='profile_id')
+    bad_profile_layers = pd.merge(layers, bad_profiles, on='profile_id')
+
+    bad_profiles.to_csv('bad_profiles.csv')
+    bad_profile_layers.to_csv('bad_layers.csv')
+
+    return layers[~layers['profile_id'].isin(bad_profiles['profile_id'])]
+
 def add_preprocessed_cols(layers, drop_zeros=True):
-    layers['mid'] = layers[['top', 'bottom']].mean(axis=1)
+    layers.loc[:, 'mid'] = layers[['top', 'bottom']].mean(axis=1)
+
+    print('{} layers with mid = 0'.format(len(layers[layers['mid'] == 0])))
+    print('{} layers with top = 0 & bottom = 0'.format(len(layers[(layers['top'] == 0) & (layers['bottom'] == 0)])))
+    print('{} layers with orgc_value_avg = 0'.format(len(layers[layers['orgc_value_avg'] == 0])))
 
     if drop_zeros:
         layers = layers[layers['mid'] > 0]
         layers = layers[layers['orgc_value_avg'] != 0]
 
-    layers['log_mid'] = np.log10(layers['mid'])
-    layers['log_bottom'] = np.log10(layers['bottom'])
-    layers['log_orgc_value_avg'] = np.log10(layers['orgc_value_avg'])
+    layers.loc[:, 'log_mid'] = np.log10(layers['mid'])
+    layers.loc[:, 'log_bottom'] = np.log10(layers['bottom'])
+    layers.loc[:, 'log_orgc_value_avg'] = np.log10(layers['orgc_value_avg'])
     return layers
 
 
@@ -54,5 +68,6 @@ def fit_linregress(layers):
     print(f'log C = {slope:.4f} d + {intercept:.4f}, R^2 = {r_value ** 2:.4f}, stderr = {std_err:.4f}')
 
 attributes, profiles, layers = load_data()
+layers = drop_bad_data(layers, profiles)
 layers = add_preprocessed_cols(layers)
 fit_linregress(layers)
