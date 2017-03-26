a0713dc65131d7b69e60be585d0418ef3cbce11c
Charles Lu <charleslu@live.com>
2017-03-25 23:58:15 -0700

    Implement removing all layers in profiles for df of layers, filtering bad data

--- analyze.py
+++ analyze.py
@@ -31,32 +31,36 @@ def visualize_layers(layers):
     plt.scatter(layers.head(5000)['top'], layers.head(5000)['orgc_value_avg'])
     plt.show()
 
-def drop_bad_data(layers, profiles):
-    bad_layers = layers[layers['top'] < 0]
+def drop_same_profile_layers(layers, profiles, bad_layers, verbose=True):
+    '''
+    Given bad_layers, drops all layers in the same profiles from layers and returns the result. 
+    '''
     bad_profiles = pd.merge(profiles, bad_layers, on='profile_id')
     bad_profile_layers = pd.merge(layers, bad_profiles, on='profile_id')
-
-    bad_profiles.to_csv('bad_profiles.csv')
-    bad_profile_layers.to_csv('bad_layers.csv')
-
+    if verbose:
+        print(f'Dropped layers: {len(bad_profile_layers)}')
     return layers[~layers['profile_id'].isin(bad_profiles['profile_id'])]
+
+
+def drop_bad_data(layers, profiles):
+    print('Dropping layers with top < 0, and layers in same profiles.')
+    layers = drop_same_profile_layers(layers, profiles, layers[layers['top'] < 0])
+
+    print('Dropping layers with orgc_value_avg = 0, and layers in same profiles.')
+    layers = drop_same_profile_layers(layers, profiles, layers[layers['orgc_value_avg'] == 0])
+
+    print('Dropping layers with top = 0 & bottom = 0, and layers in same profiles.')
+    layers = drop_same_profile_layers(layers, profiles, layers[(layers['top'] == 0) & (layers['bottom'] == 0)])
+
+    return layers
 
 def add_preprocessed_cols(layers, drop_zeros=True):
     layers.loc[:, 'mid'] = layers[['top', 'bottom']].mean(axis=1)
 
-    print('{} layers with mid = 0'.format(len(layers[layers['mid'] == 0])))
-    print('{} layers with top = 0 & bottom = 0'.format(len(layers[(layers['top'] == 0) & (layers['bottom'] == 0)])))
-    print('{} layers with orgc_value_avg = 0'.format(len(layers[layers['orgc_value_avg'] == 0])))
-
-    if drop_zeros:
-        layers = layers[layers['mid'] > 0]
-        layers = layers[layers['orgc_value_avg'] != 0]
-
-    layers.loc[:, 'log_mid'] = np.log10(layers['mid'])
-    layers.loc[:, 'log_bottom'] = np.log10(layers['bottom'])
-    layers.loc[:, 'log_orgc_value_avg'] = np.log10(layers['orgc_value_avg'])
+    layers.loc[:, 'log_mid'] = np.log10(layers.loc[:, 'mid'])
+    # layers.loc[:, 'log_bottom'] = np.log10(layers['bottom'])
+    layers.loc[:, 'log_orgc_value_avg'] = np.log10(layers.loc[:, 'orgc_value_avg'])
     return layers
-
 
 def fit_linregress(layers):
     # log-log
@@ -68,6 +72,10 @@ def fit_linregress(layers):
     print(f'log C = {slope:.4f} d + {intercept:.4f}, R^2 = {r_value ** 2:.4f}, stderr = {std_err:.4f}')
 
 attributes, profiles, layers = load_data()
+print(f'Total data points: {len(layers)}')
+print(f'Total profiles: {len(profiles)}')
+print(f'Data points with orgc_value_avg = 0: {sum(layers["orgc_value_avg"] == 0)}')
 layers = drop_bad_data(layers, profiles)
 layers = add_preprocessed_cols(layers)
+print(f'Fitting models on {len(layers)} data points.')
 fit_linregress(layers)
