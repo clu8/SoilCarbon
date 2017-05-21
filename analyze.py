72c511659f6919da38ef8527d5155d3e52ec4ce7
Charles Lu <charleslu@live.com>
2017-05-21 04:59:39 -0700

    rm unnecessary isin

--- analyze.py
+++ analyze.py
@@ -11,15 +11,13 @@ def visualize_layers(layers):
     plt.scatter(layers.head(5000)['top'], layers.head(5000)['orgc_value_avg'])
     plt.show()
 
-def drop_same_profile_layers(layers, profiles, bad_layers):
+def drop_same_profile_layers(layers, bad_layers):
     '''
     Given bad_layers, drops all layers in the same profiles from layers and returns the result. 
     '''
-    bad_profiles = pd.merge(profiles, bad_layers, on='profile_id')
-    bad_layers_mask = layers['profile_id'].isin(bad_profiles['profile_id'])
+    bad_layers_mask = layers['profile_id'].isin(bad_layers['profile_id'])
     print(f'Dropped layers: {sum(bad_layers_mask)}')
     return layers[~bad_layers_mask]
-
 
 def drop_bad_data(layers, profiles):
     bad_layer_mask = layers['top'].isnull() | layers['bottom'].isnull() | layers['orgc_value_avg'].isnull()
@@ -30,15 +28,15 @@ def drop_bad_data(layers, profiles):
 
     bad_layer_mask = layers['top'] < 0
     print(f'Layers with top < 0, and layers in same profiles: {sum(bad_layer_mask)}')
-    layers = drop_same_profile_layers(layers, profiles, layers[bad_layer_mask])
+    layers = drop_same_profile_layers(layers, layers[bad_layer_mask])
 
     bad_layer_mask = layers['orgc_value_avg'] == 0
     print(f'Layers with orgc_value_avg = 0: {sum(bad_layer_mask)}')
-    layers = drop_same_profile_layers(layers, profiles, layers[bad_layer_mask])
+    layers = drop_same_profile_layers(layers, layers[bad_layer_mask])
 
     bad_layer_mask = (layers['top'] == 0) & (layers['bottom'] == 0)
     print(f'Layers with top = 0 & bottom = 0: {sum(bad_layer_mask)}')
-    layers = drop_same_profile_layers(layers, profiles, layers[bad_layer_mask])
+    layers = drop_same_profile_layers(layers, layers[bad_layer_mask])
 
     print('Layers in profiles <40 cm with all orgc > 17%.')
     print(f'Dropped layers: {sum(layers["peatland_manual"] == "BadData")}')
@@ -74,16 +72,15 @@ def fit_linregress_per_biome(layers):
             print('No data!')
 
 def fit_models(layers, profiles):
-    peatland_mask = profiles['soil_type_all'] == 'peatland'
-    permafrost_mask = profiles['soil_type_all'] == 'permafrost'
+    peatland_mask = layers['soil_type_all'] == 'peatland'
+    permafrost_mask = layers['soil_type_all'] == 'permafrost'
     other_soils_mask = ~peatland_mask & ~permafrost_mask
 
     for mask, soil_type in ((slice(None), 'All soils'),
                             (peatland_mask, 'Peatlands'),
                             (permafrost_mask, 'Permafrost'),
                             (other_soils_mask, 'Other soils')):
-        layers_mask = layers['profile_id'].isin(profiles[mask]['profile_id'])
-        layers_subset = layers[layers_mask]
+        layers_subset = layers[mask]
         print(f'\n====== SOIL TYPE: {soil_type} ======')
         print(f'Fitting models on {len(layers_subset)} layers.')
         fit_linregress(layers_subset)
